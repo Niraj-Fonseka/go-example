cccc83b4fbdac8e65ff6d9ccbae2bec0cfd211eb
Niraj-Fonseka <fonseka.live@utexas.edu>
2018-07-15 22:25:52 -0500

    Simplifying

--- main.go
+++ main.go
@@ -2,89 +2,30 @@ package main
 
 import (
 	"log"
-	"time"
 
 	sdk "github.com/gaia-pipeline/gosdk"
 )
 
-func CreateUser() error {
-	log.Println("CreateUser has been started!")
+// This is one job. Add more if you want.
+func DoSomethingAwesome() error {
+	log.Println("This output will be streamed back to gaia and will be displayed in the pipeline logs.")
 
-	// lets sleep to simulate that we do something
-	time.Sleep(15 * time.Second)
-
-	log.Println("CreateUser has been finished!")
-
-	return nil
-}
-
-func MigrateDB() error {
-	log.Println("MigrateDB has been started!")
-
-	// lets sleep to simulate that we do something
-	time.Sleep(15 * time.Second)
-
-	log.Println("MigrateDB has been finished!")
-
-	return nil
-}
-
-func CreateNamespace() error {
-	log.Println("CreateNamespace has been started!")
-
-	// lets sleep to simulate that we do something
-	time.Sleep(15 * time.Second)
-
-	log.Println("CreateNamespace has been finished!")
-
-	return nil
-}
-
-func CreateDeployment() error {
-	log.Println("CreateDeployment has been started!")
-
-	// lets sleep to simulate that we do something
-	time.Sleep(15 * time.Second)
-
-	log.Println("CreateDeployment has been finished!")
-
-	return nil
-}
-
-func CreateService() error {
-	log.Println("CreateService has been started!")
-
-	// lets sleep to simulate that we do something
-	time.Sleep(15 * time.Second)
-
-	log.Println("CreateService has been finished!")
-
-	return nil
-}
-
-func CreateIngress() error {
-	log.Println("CreateIngress has been started!")
-
-	// lets sleep to simulate that we do something
-	time.Sleep(15 * time.Second)
-
-	log.Println("CreateIngress has been finished!")
-
-	return nil
-}
-
-func Cleanup() error {
-	log.Println("Cleanup has been started!")
-
-	// lets sleep to simulate that we do something
-	time.Sleep(15 * time.Second)
-
-	log.Println("Cleanup has been finished!")
-
+	// An error occured? Return it back so gaia knows that this job failed.
 	return nil
 }
 
 func main() {
+	jobs := sdk.Jobs{
+		sdk.Job{
+			Handler:     DoSomethingAwesome,
+			Title:       "DoSomethingAwesome",
+			Description: "This job does something awesome.",
+
+			// Increase the priority if this job should be executed later than other jobs.
+			Priority: 0,
+		},
+	}
+
 	// Serve
 	if err := sdk.Serve(jobs); err != nil {
 		panic(err)
